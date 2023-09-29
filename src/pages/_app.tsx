34afb1de68c2e722dbad1b00416d576d3ec16561
Krish120003 <krish120003@gmail.com>
2023-09-29 02:26:37 -0400

    feat: add light/dark theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import { type AppType } from "next/app";
+import { ThemeProvider } from "~/components/theme-provider";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "~/components/ui/toaster";
+import { ModeToggle } from "~/components/ui/theme-picker";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="absolute right-0 top-0 p-4">
+        <ModeToggle />
+      </div>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 };
 
